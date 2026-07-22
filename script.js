b8ded5fc5f3c95bb7650dd1a75496fe0b87408de
ruthhhs <ruthseptrianas@gmail.com>
2026-07-22 17:29:40 +0700

    add refresh btn

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // variables
 const video = document.getElementById('video');
+const control = document.getElementById('control');
 const captureBtn = document.getElementById('capture-btn');
 const photoList = document.getElementById('photo-list');
 
@@ -39,18 +40,41 @@ captureBtn.addEventListener("click", () => {
         remainingSessions--;
 
         if (remainingSessions === 0) {
-            captureBtn.textContent = 'Download here !';
+            captureBtn.style.display = "none";
+
+            const btn1 = document.createElement('button');
+            btn1.id = 'downloadBtn';
+            btn1.className = 'btn';
+            btn1.innerText = 'Download here !';
+            btn1.addEventListener("click", downloadPhoto);
+            
+            const btn2 = document.createElement('button');
+            btn2.id = 'refreshBtn';
+            btn2.className = 'btn';
+            btn2.innerText = 'Refresh';
+            btn2.addEventListener("click", resetSession);
+
+            control.appendChild(btn1);
+            control.appendChild(btn2);
         }
-
-    } else {
-        // download fuction
-        html2canvas(document.getElementById("photo-container"))
-        .then(canvas => {
-            const a = document.createElement("a");
-            a.download = "photo.png";
-            a.href = canvas.toDataURL("image/png");
-            a.click();
-        });
-
     }
 });
+
+function downloadPhoto() {
+    html2canvas(document.getElementById("photo-container"))
+    .then(canvas => {
+        const a = document.createElement("a");
+        a.download = "photo.png";
+        a.href = canvas.toDataURL("image/png");
+        a.click();
+    });
+}
+
+function resetSession() {
+    remainingSessions = 3;
+    captureBtn.style.display = "block";
+
+    document.getElementById("downloadBtn")?.remove();
+    document.getElementById("refreshBtn")?.remove();
+    document.getElementById("photo-list").innerHTML = "";
+}
