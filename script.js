8dbc70ed932c1e21d90c343a27c37581c5b558a1
ruthhhs <ruthseptrianas@gmail.com>
2026-07-23 18:35:12 +0700

    anjirla 3 jam cuma fix slider pages

--- script.js
+++ script.js
@@ -2,9 +2,58 @@
 const video = document.getElementById('video');
 const control = document.getElementById('control');
 const captureBtn = document.getElementById('capture-btn');
+const photoContainer = document.getElementById('photo-container');
 const photoList = document.getElementById('photo-list');
+const comment = document.getElementById('comment');
+const pages = document.getElementById('pages');
+const backBtn = document.getElementById('back-btn');
+const downloadBtn = document.getElementById('download-btn');
 
-// activate camera
+const resetColor = "rgb(246, 247, 242)";
+let currentPage = 0;
+
+startCamera();
+let photosPerSession = 3;
+
+captureBtn.addEventListener("click", () => {
+    if (photosPerSession > 0) {
+        createPhoto();
+        photosPerSession--;
+
+        if (photosPerSession === 0) {
+            captureBtn.style.display = "none";
+
+            const btn1 = document.createElement('button');
+            btn1.id = 'costumizeBtn';
+            btn1.className = 'btn';
+            btn1.innerText = 'Yes abseloutly!';
+            btn1.onclick = () => goToPage(currentPage + 1);
+            
+            const btn2 = document.createElement('button');
+            btn2.id = 'refreshBtn';
+            btn2.className = 'btn';
+            btn2.innerText = "No, I'll retake";
+            btn2.onclick = () => resetSession();
+
+            control.appendChild(btn1);
+            control.appendChild(btn2);
+
+            comment.textContent='Great! Would you like to costumize?';
+        }
+    }
+});
+
+backBtn.onclick = () => goToPage(currentPage - 1);
+downloadBtn.onclick = () => downloadPhoto();
+
+pickColor.addEventListener("input", (e) => {
+    let color = e.target.value;
+    photoContainer.style.backgroundColor = color;
+    photoList.style.backgroundColor = color;
+});
+
+// ===== FUNCTIONS =====
+
 async function startCamera() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({
@@ -15,51 +64,40 @@ async function startCamera() {
         console.error("Cannot access camera :(", err);
     }
 }
-startCamera();
+function createPhoto(){
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
 
-// capture button logic
+    // create image
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    const dataURL = canvas.toDataURL("image/png");
 
-let remainingSessions = 3;
+    // listing images into photo-list
+    const img = document.createElement('img');
+    img.src = dataURL;
+    photoList.appendChild(img);
+}
+function goToPage(page){
+    currentPage = page;
+    pages.style.transform = `translateY(-${currentPage * pages.clientHeight}px)`;
+}
+function resetSession() {
+    photosPerSession = 3;
+    captureBtn.style.display = "block";
 
-captureBtn.addEventListener("click", () => {
-    if (remainingSessions > 0) {
-        const canvas = document.createElement("canvas");
-        const context = canvas.getContext("2d");
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-    
-        // create image
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        const dataURL = canvas.toDataURL("image/png");
-    
-        // listing images into photo-list
-        const img = document.createElement('img');
-        img.src = dataURL;
-        photoList.appendChild(img);
+    document.getElementById("costumizeBtn")?.remove();
+    document.getElementById("refreshBtn")?.remove();
+    document.getElementById("pickColor")?.remove();
+    document.getElementById("photo-list").innerHTML = "";
 
-        remainingSessions--;
+    comment.textContent='Smile sweetheart <3';
 
-        if (remainingSessions === 0) {
-            captureBtn.style.display = "none";
-
-            const btn1 = document.createElement('button');
-            btn1.id = 'downloadBtn';
-            btn1.className = 'btn';
-            btn1.innerText = 'Download here !';
-            btn1.addEventListener("click", downloadPhoto);
-            
-            const btn2 = document.createElement('button');
-            btn2.id = 'refreshBtn';
-            btn2.className = 'btn';
-            btn2.innerText = 'Refresh';
-            btn2.addEventListener("click", resetSession);
-
-            control.appendChild(btn1);
-            control.appendChild(btn2);
-        }
-    }
-});
-
+    photoContainer.style.backgroundColor = resetColor;
+    photoList.style.backgroundColor = resetColor;
+    colorPicker.value = resetColor;
+}
 function downloadPhoto() {
     html2canvas(document.getElementById("photo-container"))
     .then(canvas => {
@@ -68,13 +106,4 @@ function downloadPhoto() {
         a.href = canvas.toDataURL("image/png");
         a.click();
     });
-}
-
-function resetSession() {
-    remainingSessions = 3;
-    captureBtn.style.display = "block";
-
-    document.getElementById("downloadBtn")?.remove();
-    document.getElementById("refreshBtn")?.remove();
-    document.getElementById("photo-list").innerHTML = "";
 }
